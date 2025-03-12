7b77a0e68ee8d97999a89dec33af13a5d810bdf7
aquestry <antonkonig691@gmail.com>
2025-03-12 13:37:49 +0100

    Update
    removed some bad logic and cleaned up ad bit

--- plugins/starfield/commands/lobby.go
+++ plugins/starfield/commands/lobby.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"github.com/aquestry/starfield/plugins/starfield/events"
 	"go.minekube.com/brigodier"
 	"go.minekube.com/common/minecraft/component"
 	"go.minekube.com/gate/pkg/command"
@@ -10,17 +9,13 @@ import (
 
 func LobbyCommand() brigodier.LiteralNodeBuilder {
 	lobby := command.Command(func(ctx *command.Context) error {
-		player, ok := ctx.Source.(proxy.Player)
+		_, ok := ctx.Source.(proxy.Player)
 		if !ok {
 			return ctx.SendMessage(&component.Text{
 				Content: "Only players can use this command.",
 			})
 		}
 		msg := "Connecting you to a lobby..."
-		fine := events.Lobby(player)
-		if !fine {
-			msg = "You are already on a lobby."
-		}
 		return ctx.SendMessage(&component.Text{Content: msg})
 	})
 	return brigodier.Literal("lobby").Executes(lobby)
